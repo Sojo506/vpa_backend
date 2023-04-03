5c1681c2fd4a6242335665f0405774cd0b98903b
Sojo506 <fabiansojowork@gmail.com>
2023-04-03 09:54:35 -0600

    config emails

--- utils/emailForgotPassword.js
+++ utils/emailForgotPassword.js
@@ -4,6 +4,7 @@ const emailForgotPassword = async (data) => {
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
